b43e5b7a737aa8b298cfdda9c0a4b2cabb809050
Dany7788 <dany.315920@gmail.com>
2020-06-14 18:52:15 +0400

    Комм №1

---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + y);
+        }
+    }
+}
